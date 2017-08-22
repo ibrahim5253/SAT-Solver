37972cc024d3f95933770991c8b68dd29e39c588
Ibrahim Dalal <ibrahim@Ibrahims-MacBook-Air.local>
2017-08-22 16:49:40 +0530

    seems to be working

--- CS14BTECH11041-sat.cpp
+++ CS14BTECH11041-sat.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 
 #define pb push_back
+#define ff first
+#define ss second
+
 
 int const N = 501;
 int const M = 10001;
 
 using namespace std;
 using vi=vector<int>;
-using clause=set<int>;
-using boolean_formula=list<clause>;
+//using clause=set<int>;
+//using boolean_formula=list<clause>;
 
-vector<bitset<N>> literals;
-//vector<bitset<M>> clauses;
+vector<set<int>> Literals;
+vector<vector<int>> clauses;
 
-int clause_state[M];
+bool is_satisfied[M];
 
 int n, m;
+int n_active_clause;
 
 void input() 
 {
@@ -29,8 +33,9 @@ void input()
     string p, _cnf;
     ss>>p>>_cnf>>n>>m;
 
-    literals.resize(2*m+1, bitset<N>()),
-//    clauses.resize(2*n+1, bitset<M>());
+    Literals.resize(m+1, set<int>()),
+    clauses.resize(n+1, vi()),
+    n_active_clause=m;
 
     for (int i=1; i<=m; ++i) {
         getline(cin,s);
@@ -38,84 +43,123 @@ void input()
         stringstream ss(s);
         int j; ss>>j;
         while(j!=0) {
-            if (j<0) literals[(i<<1)-1][-j]=1;//, clauses[(-j<<1)-1][i]=1;
-            else literals[i<<1][j]=1;//, clauses[j<<1][i]=1;
+            Literals[i].insert(j);
+            clauses[abs(j)].pb(j>0?i:-i);
             ss>>j;
         }
     }
 }
 
-bool unit_resolution(boolean_formula& cnf, vi& res)
+bool unit_resolution(vi& res, vi& removed, map<int, list<int>>& erased)
 {
     queue<int> q;
 
+    bool seen[N]={false};
     for (int i=1; i<=m; ++i) {
-       if (literals[(i<<1)-1].count()+literals[i<<1].count()==1) 
-           for (int j=1; j<=n; ++j) {
-               if (literals[(i<<1)-1][j]) q.push(-j);
-               else if (literals[i<<1][j]) q.push(j);
-           }
+       if (is_satisfied[i]) continue; 
+       if (Literals[i].size()==1) {
+           auto j =*Literals[i].begin();
+           if (!seen[abs(j)]) q.push(j), seen[abs(j)]=true;
+       }
     }
 
     while(!q.empty()) {
 
-        auto x=q.front();
+        auto j=q.front();
         q.pop();
-        res.pb(x);
+        res.pb(j);
 
-        for (auto it=v[abs(x)].begin(), stop=v[abs(x)].end(); it!=stop; ++it) {
+        for (auto i: clauses[abs(j)]) {
 
-            auto& c = **it;
+            if (is_satisfied[abs(i)]) continue;
 
-            if (c == cnf.end()) continue;
-
-            c->erase(-x);
-            if (c->count(x)) cnf.erase(c), c = cnf.end();
-            else if (c->size()==0) return false;
-            else if (c->size()==1 and !seen[abs(*(c->begin()))]) 
-                q.push(*(c->begin())), seen[abs(*(c->begin()))]=true;
+            if (j*i > 0) removed.pb(abs(i)), is_satisfied[abs(i)]=true, --n_active_clause;
+            else {
+                i = abs(i);
+                erased[-j].pb(i), Literals[i].erase(-j);
+                if (Literals[i].size()==0) return false;
+                else if (Literals[i].size()==1) {
+                    auto x  = *Literals[i].begin();
+                    if (!seen[abs(x)]) q.push(x), seen[abs(x)]=true;
+                }
+            }
         } 
     }
 
     return true;
 }
 
-boolean_formula condition(const boolean_formula& cnf, int l)
+void condition(int l, vi& removed, map<int, list<int>>& erased)
 {
-    boolean_formula new_cnf(cnf);
-
-    for (auto it=new_cnf.begin(), stop=new_cnf.end(); it!=stop; ) {
-        it->erase(-l);
-        if (it->count(l)) it = new_cnf.erase(it);
-        else ++it;
+    for (auto i: clauses[abs(l)]) {
+        if (is_satisfied[abs(i)]) continue;
+        
+        if (l*i > 0) removed.pb(abs(i)), is_satisfied[abs(i)]=true, --n_active_clause;
+        else {
+            i = abs(i);
+            erased[-l].pb(i), Literals[i].erase(-l);
+        }
     }
-
-    return new_cnf;
 }
 
-
-bool solve(boolean_formula&& cnf, vi& I)
+void backtrack(vi& removed, map<int, list<int>>& erased)
 {
-    if (!unit_resolution(cnf, I))
-        return false;
+    for (auto i: removed)
+        is_satisfied[i]=false, ++n_active_clause;
 
-    if (cnf.empty())
-        return true;
+    for (auto& x: erased)
+        for (auto& i: x.ss)
+            Literals[i].insert(x.ff);
+}
 
-    auto literal = *(cnf.front().begin());
+int choose_literal()
+{
+    for (int i=1; i<=m; ++i)
+        if (!is_satisfied[i]) return *Literals[i].begin();
+}
 
+bool solve(vi&, int d);
+
+bool solve_branch(int l, vi& I, int d)
+{
     vi _I;
-    if (solve(condition(cnf,literal), _I)) {
+    vi removed_cond;
+    map<int, list<int>> erased_cond;
+
+    condition(l, removed_cond, erased_cond);
+    if (solve(_I, d)) {
         I.insert(I.end(), _I.begin(), _I.end());
-        I.pb(literal);
+        I.pb(l);
         return true;
     }
-    _I.clear();
-    if (solve(condition(cnf,-literal), _I)) {
-        I.insert(I.end(), _I.begin(), _I.end());
-        I.pb(-literal);
+
+    backtrack(removed_cond, erased_cond);
+    return false;
+}
+
+bool solve(vi& I, int d=1)
+{
+    vi removed_unit;
+    map<int, list<int>> erased_unit;
+
+    if (!unit_resolution(I, removed_unit, erased_unit)) {
+        backtrack(removed_unit, erased_unit);
+        return false;
+    }
+
+    if (n_active_clause == 0)
+        return true;
+
+    auto l = choose_literal();
+
+    if (solve_branch(l, I, d+1)) {
         return true;
     }
+    else if (solve_branch(-l, I, d+1)) {
+        return true;
+    }
+
+    backtrack(removed_unit, erased_unit);
     return false;
 }
 
@@ -123,11 +167,9 @@ bool foo(int a, int b) {return abs(a) < abs(b);}
 
 int main()
 {
-    boolean_formula cnf;
-    input(cnf);
-    cout<<N<<endl;
+    input();
     vi v;
-    if (solve(move(cnf), v)) {
+    if (solve(v)) {
         cout<<"SAT\n";
         sort(v.begin(), v.end(), foo);
         int i=1;
@@ -135,7 +177,7 @@ int main()
             while(i<abs(*it)) cout<<i++<<" ";
             cout<<*it<<" ";
         }
-        while(i<=N) cout<<i++<<" ";
+        while(i<=n) cout<<i++<<" ";
         cout<<"0";
     }
     else cout<<"UNSAT";
